c18608d3c85b5ec1d3e52918db3df85e1f8fb6a9
yeojin <lyeo7272@gmail.com>
2023-02-02 17:25:51 +0900

    게시글 수정, 삭제 화면구현 / 삭제 API

--- src/main/java/com/example/demo/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/example/demo/springboot/web/dto/PostsResponseDto.java
@@ -12,6 +12,7 @@ public class PostsResponseDto {
     private String author;
 
     public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
