78ec2c5c147c8198bc39987f74081d4c0d739efc
yeojin <lyeo7272@gmail.com>
2023-02-02 10:17:09 +0900

    게시글 등록 API 개발, 등록 API 테스트 코드 작성

---
+++ src/main/java/com/example/demo/springboot/service/PostsService.java
@@ -0,0 +1,17 @@
+package com.example.demo.springboot.service;
+
+import com.example.demo.springboot.domain.posts.PostsRepository;
+import com.example.demo.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
