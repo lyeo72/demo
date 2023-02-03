8e45500d04337353d04177aa25aa0abc12fddb32
yeojin <lyeo7272@gmail.com>
2023-02-03 15:46:17 +0900

    naver 로그인 추가

--- src/main/java/com/example/demo/springboot/web/IndexController.java
+++ src/main/java/com/example/demo/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo.springboot.web;
 
+import com.example.demo.springboot.config.auth.LoginUser;
 import com.example.demo.springboot.config.auth.dto.SessionUser;
 import com.example.demo.springboot.service.PostsService;
 import com.example.demo.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
